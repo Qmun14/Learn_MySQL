1231852ede1735c7af12d5f84999cabbf58d0360
Qmun14 <mamunramdhan@gmail.com>
2022-03-02 07:11:41 +0700

    Menambahkan file MySQL_Cheat_Sheet

---
+++ mysql/MySQL_CheatSheet_Syntax.sql
@@ -0,0 +1,49 @@
+Semua perintah di bawah ini akan dijalankan dari terminal (command line).
+
+Mulailah dengan menjalankan layanan mysql dan membuka shell mysql, ini dapat dilakukan secara bersamaan dengan satu perintah:
+mysql-ctl cli
+Jika ini berhasil maka Anda akan melihat mysql> di terminal Anda. Anda sekarang dapat memasukkan perintah MySQL berikut:
+
+Daftar semua database yang ada:
+SHOW DATABASES;
+Jika Anda melihat database Anda, buka dengan:
+USE <db_name>;
+
+Mendapatkan Informasi Tentang Database dan Tabel
+Anda sebelumnya telah melihat SHOW DATABASES, yang mencantumkan database yang dikelola oleh server. 
+Untuk mengetahui database mana yang sedang dipilih, gunakan fungsi DATABASE() :
+mysql> SELECT DATABASE();
++------------+
+| DATABASE() |
++------------+
+| menagerie  |
++------------+
+
+Jika Anda belum memilih database apa pun, hasilnya adalah NULL.
+
+Untuk mengetahui tabel apa yang berisi database default (misalnya, ketika Anda tidak yakin tentang nama tabel), gunakan pernyataan ini:
+mysql> SHOW TABLES;
++---------------------+
+| Tables_in_menagerie |
++---------------------+
+| event               |
+| pet                 |
++---------------------+
+
+Nama kolom dalam output yang dihasilkan oleh pernyataan ini selalu Tables_in_db_name, di mana db_name adalah nama database. Lihat Bagian 13.7.7.39, “Pernyataan SHOW TABLES”, untuk informasi lebih lanjut.
+
+Jika Anda ingin mengetahui tentang struktur tabel, pernyataan DESCRIBE berguna; ini menampilkan informasi tentang masing-masing kolom tabel:
+mysql> DESCRIBE pet;
++---------+-------------+------+-----+---------+-------+
+| Field   | Type        | Null | Key | Default | Extra |
++---------+-------------+------+-----+---------+-------+
+| name    | varchar(20) | YES  |     | NULL    |       |
+| owner   | varchar(20) | YES  |     | NULL    |       |
+| species | varchar(20) | YES  |     | NULL    |       |
+| sex     | char(1)     | YES  |     | NULL    |       |
+| birth   | date        | YES  |     | NULL    |       |
+| death   | date        | YES  |     | NULL    |       |
++---------+-------------+------+-----+---------+-------+
+DESC adalah kependekan dari DESCRIBE. Lihat Bagian 13.8.1, “MENJELASKAN Pernyataan”, untuk informasi lebih lanjut.
+
+
