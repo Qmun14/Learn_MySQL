4aecacf06d0b2355fd131e879e86a38121183896
Qmun14 <mamunramdhan@gmail.com>
2022-03-02 11:45:23 +0700

    Menyelesaikan UPDTAE command Challange

--- mysql/MySQL_CheatSheet_Syntax.sql
+++ mysql/MySQL_CheatSheet_Syntax.sql
@@ -122,3 +122,28 @@ UPDATE cats SET breed='Shorthair' WHERE breed='Tabby';
 Another update:
 
 UPDATE cats SET age=14 WHERE name='Misty'; 
+
+-- =================
+CODE: UPDATE Challenges Solution
+CODE: Update Challenges Solution
+SELECT * FROM cats WHERE name='Jackson';
+ 
+UPDATE cats SET name='Jack' WHERE name='Jackson';
+ 
+SELECT * FROM cats WHERE name='Jackson';
+ 
+SELECT * FROM cats WHERE name='Jack';
+ 
+SELECT * FROM cats WHERE name='Ringo';
+ 
+UPDATE cats SET breed='British Shorthair' WHERE name='Ringo';
+ 
+SELECT * FROM cats WHERE name='Ringo';
+ 
+SELECT * FROM cats;
+ 
+SELECT * FROM cats WHERE breed='Maine Coon';
+ 
+UPDATE cats SET age=12 WHERE breed='Maine Coon';
+ 
+SELECT * FROM cats WHERE breed='Maine Coon';
