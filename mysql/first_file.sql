666beeb6b78830e05143142736fbd4c53031b24e
Qmun14 <mamunramdhan@gmail.com>
2022-03-03 11:34:26 +0700

    Menyelesaikan Mater -- Read MySQL from file di CLI Terminal --

---
+++ mysql/first_file.sql
@@ -0,0 +1,6 @@
+CREATE TABLE cats (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(100),
+    breed VARCHAR(100),
+    age int
+);
---
+++ mysql/testing/insert.sql
@@ -0,0 +1,24 @@
+INSERT INTO cats (
+    name,
+    breed,
+    age
+) VALUES (
+    'Charlie', 
+    'Britsh Short',
+    17
+);
+
+INSERT INTO cats(
+    name,
+    breed,
+    age 
+) VALUES (
+    'Connie',
+    'Persian',
+    10
+);
+
+SELECT * FROM cats;
+
+-- source testing/insert.sql  """"Jalankan di Terminal/ CLi"""""
+
