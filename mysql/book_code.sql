0a9348c9970f8fa84b02e791d7c4e27f8d70e362
Qmun14 <mamunramdhan@gmail.com>
2022-03-03 22:09:23 +0700

    Menyelesaikan Materi -- SQL String Function (SUBSTRING()) -- dengan Implementasinya

---
+++ mysql/book_code.sql
@@ -0,0 +1,13 @@
+SELECT CONCAT
+    (
+        SUBSTRING(title, 1, 10),
+        '...'
+    )
+FROM books;
+
+SELECT CONCAT
+    (
+        SUBSTRING(title, 1, 10),
+        '...'
+    ) AS 'short title'
+FROM books;
--- mysql/book_data.sql
+++ mysql/book_data.sql
@@ -117,3 +117,47 @@ SELECT CONCAT(title, '-', author_fname, '-', author_lname) FROM books;
 SELECT 
     CONCAT_WS(' - ', title, author_fname, author_lname) 
 FROM books;
+
+-- =================================================================================================================================
+
+CODE: Introducing SUBSTRING
+SELECT SUBSTRING('Hello World', 1, 4);
+ 
+SELECT SUBSTRING('Hello World', 7);
+ 
+SELECT SUBSTRING('Hello World', 3, 8);
+ 
+SELECT SUBSTRING('Hello World', 3);
+ 
+SELECT SUBSTRING('Hello World', -3);
+ 
+SELECT SUBSTRING('Hello World', -7);
+ 
+SELECT title FROM books;
+ 
+SELECT SUBSTRING("Where I'm Calling From: Selected Stories", 1, 10);
+ 
+SELECT SUBSTRING(title, 1, 10) FROM books;
+ 
+SELECT SUBSTRING(title, 1, 10) AS 'short title' FROM books;
+ 
+SELECT SUBSTR(title, 1, 10) AS 'short title' FROM books;
+ 
+SELECT CONCAT
+    (
+        SUBSTRING(title, 1, 10),
+        '...'
+    )
+FROM books;
+ 
+source book_code.sql
+ 
+SELECT CONCAT
+    (
+        SUBSTRING(title, 1, 10),
+        '...'
+    ) AS 'short title'
+FROM books;
+ 
+source book_code.sql
+
