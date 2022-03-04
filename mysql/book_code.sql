4e451f9b3d281b18e3fbfbe88af7fae87385c050
Qmun14 <mamunramdhan@gmail.com>
2022-03-04 13:03:40 +0700

    Menyelesaikan Materi -- SQL STRING FUNCTION (CHAR_LENGTH()) dan Implementasinya --

--- mysql/book_code.sql
+++ mysql/book_code.sql
@@ -47,3 +47,31 @@ SELECT CONCAT(author_fname, REVERSE (author_fname)) AS NAME FROM books;
 -- | DaviddivaD     |
 -- +----------------+
 -- 16 rows in set (0.00 sec)
+
+# "Eggs is 4 Characters long"
+
+SELECT
+  CONCAT(author_lname, ' is ', CHAR_LENGTH(author_lname), ' Caharacters long') AS "Length Characters Names"
+FROM books;
+
+-- +---------------------------------------+
+-- | Length Characters Last Names          |
+-- +---------------------------------------+
+-- | Lahiri is 6 Caharacters long          |
+-- | Gaiman is 6 Caharacters long          |
+-- | Gaiman is 6 Caharacters long          |
+-- | Lahiri is 6 Caharacters long          |
+-- | Eggers is 6 Caharacters long          |
+-- | Eggers is 6 Caharacters long          |
+-- | Chabon is 6 Caharacters long          |
+-- | Smith is 5 Caharacters long           |
+-- | Eggers is 6 Caharacters long          |
+-- | Gaiman is 6 Caharacters long          |
+-- | Carver is 6 Caharacters long          |
+-- | Carver is 6 Caharacters long          |
+-- | DeLillo is 7 Caharacters long         |
+-- | Steinbeck is 9 Caharacters long       |
+-- | Foster Wallace is 14 Caharacters long |
+-- | Foster Wallace is 14 Caharacters long |
+-- +---------------------------------------+
+-- 16 rows in set (0.00 sec)
--- mysql/book_data.sql
+++ mysql/book_data.sql
@@ -208,3 +208,14 @@ SELECT REVERSE(author_fname) FROM books;
 SELECT CONCAT('woof', REVERSE('woof'));
  
 SELECT CONCAT(author_fname, REVERSE(author_fname)) FROM books;
+
+-- =========================================================================================================================================================
+
+CODE: Working with CHAR LENGTH
+SELECT CHAR_LENGTH('Hello World');
+ 
+SELECT author_lname, CHAR_LENGTH(author_lname) AS 'length' FROM books;
+ 
+SELECT CONCAT(author_lname, ' is ', CHAR_LENGTH(author_lname), ' characters long') FROM books;
+
+Resource: sql-format.com
