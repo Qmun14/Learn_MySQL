632f9f0998b288973c64ffc1fc3933d5033bb41a
Qmun14 <mamunramdhan@gmail.com>
2022-03-04 11:24:34 +0700

    Menyelesaikan Materi -- SQL String Function (REPLACE()) -- dan Implelemtasinya

--- mysql/book_code.sql
+++ mysql/book_code.sql
@@ -11,3 +11,15 @@ SELECT CONCAT
         '...'
     ) AS 'short title'
 FROM books;
+
+# SELECT 
+#         CONCAT
+#     (
+#         SUBSTRING(title, 1, 10)
+#         , "..."
+#     )    AS "Short Title" 
+# FROM books;
+
+SELECT 
+    SUBSTRING(REPLACE (title, 'e', '3'), 1,10) AS "Weird String"
+FROM books;
