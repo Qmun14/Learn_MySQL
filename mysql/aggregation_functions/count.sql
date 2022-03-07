fc39e2b6623d46137f17706eee20c7a4c89db2bf
Qmun14 <mamunramdhan@gmail.com>
2022-03-07 15:34:30 +0700

    mencoba menggabungkan AGGREGATION dan Refining selections

--- mysql/aggregation_functions/count.sql
+++ mysql/aggregation_functions/count.sql
@@ -65,3 +65,30 @@ SELECT COUNT(*) FROM books WHERE title LIKE '%the%';
 -- |        6 |
 -- +----------+
 -- 1 row in set (0.00 sec)
+
+SELECT
+  CONCAT(' In year ', released_year, ' ', COUNT(*), ' book(s) released') AS 'List Year Relseased'
+FROM books
+GROUP BY released_year;
+-- mysql> mysql> source aggregation_functions/count.sql;
+-- +----------------------------------+
+-- | List Year Relseased              |
+-- +----------------------------------+
+-- |  In year 1945 1 book(s) released |
+-- |  In year 1981 1 book(s) released |
+-- |  In year 1985 1 book(s) released |
+-- |  In year 1989 1 book(s) released |
+-- |  In year 1996 1 book(s) released |
+-- |  In year 2000 1 book(s) released |
+-- |  In year 2001 3 book(s) released |
+-- |  In year 2003 2 book(s) released |
+-- |  In year 2004 1 book(s) released |
+-- |  In year 2005 1 book(s) released |
+-- |  In year 2010 1 book(s) released |
+-- |  In year 2012 1 book(s) released |
+-- |  In year 2013 1 book(s) released |
+-- |  In year 2014 1 book(s) released |
+-- |  In year 2016 1 book(s) released |
+-- |  In year 2017 1 book(s) released |
+-- +----------------------------------+
+-- 16 rows in set (0.00 sec)
