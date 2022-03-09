658f4bd230989a2f3a2063b29eb2e83a1872bf93
Qmun14 <mamunramdhan@gmail.com>
2022-03-09 19:59:56 +0700

    Menyelesaikan Materi -- SQL LOGIC Functions ( IN, NOT IN ) -- dan Implementasinya

--- mysql/Logical Opertaors/Notes.sql
+++ mysql/Logical Opertaors/Notes.sql
@@ -132,3 +132,137 @@ AND CAST('2000-01-01' AS datetime);
 -- # +-------+---------------------+
 -- # 1 row in set (0.00 sec)
 
+-- # ====================================================== logical IN, NOT IN ======================================================
+SELECT 
+    title, 
+    author_lname 
+FROM books
+WHERE author_lname='Carver' OR
+      author_lname='Lahiri' OR
+      author_lname='Smith';
+-- mysql> source Logical Operators/Notes.sql;
+-- +-----------------------------------------------------+--------------+
+-- | title                                               | author_lname |
+-- +-----------------------------------------------------+--------------+
+-- | The Namesake                                        | Lahiri       |
+-- | Interpreter of Maladies                             | Lahiri       |
+-- | Just Kids                                           | Smith        |
+-- | What We Talk About When We Talk About Love: Stories | Carver       |
+-- | Where I'm Calling From: Selected Stories            | Carver       |
+-- +-----------------------------------------------------+--------------+
+-- 5 rows in set (0.01 sec)
+
+SELECT title, author_lname FROM books
+WHERE author_lname IN ('Carver', 'Lahiri', 'Smith');
+-- mysql> source Logical Operators/Notes.sql;
+-- +-----------------------------------------------------+--------------+
+-- | title                                               | author_lname |
+-- +-----------------------------------------------------+--------------+
+-- | The Namesake                                        | Lahiri       |
+-- | Interpreter of Maladies                             | Lahiri       |
+-- | Just Kids                                           | Smith        |
+-- | What We Talk About When We Talk About Love: Stories | Carver       |
+-- | Where I'm Calling From: Selected Stories            | Carver       |
+-- +-----------------------------------------------------+--------------+
+-- 5 rows in set (0.00 sec)
+
+SELECT title, released_year FROM books
+WHERE released_year IN (2017, 1985);
+-- mysql> source Logical Operators/Notes.sql;
+-- +----------------------+---------------+
+-- | title                | released_year |
+-- +----------------------+---------------+
+-- | White Noise          |          1985 |
+-- | Lincoln In The Bardo |          2017 |
+-- +----------------------+---------------+
+-- 2 rows in set (0.00 sec)
+
+SELECT title, released_year FROM books
+WHERE released_year != 2000 AND
+      released_year != 2002 AND
+      released_year != 2004 AND
+      released_year != 2006 AND
+      released_year != 2008 AND
+      released_year != 2010 AND
+      released_year != 2012 AND
+      released_year != 2014 AND
+      released_year != 2016;
+-- mysql> source Logical Operators/Notes.sql;
+-- +-----------------------------------------------------+---------------+
+-- | title                                               | released_year |
+-- +-----------------------------------------------------+---------------+
+-- | The Namesake                                        |          2003 |
+-- | American Gods                                       |          2001 |
+-- | Interpreter of Maladies                             |          1996 |
+-- | The Circle                                          |          2013 |
+-- | A Heartbreaking Work of Staggering Genius           |          2001 |
+-- | Coraline                                            |          2003 |
+-- | What We Talk About When We Talk About Love: Stories |          1981 |
+-- | Where I'm Calling From: Selected Stories            |          1989 |
+-- | White Noise                                         |          1985 |
+-- | Cannery Row                                         |          1945 |
+-- | Consider the Lobster                                |          2005 |
+-- | fake_book                                           |          2001 |
+-- | Lincoln In The Bardo                                |          2017 |
+-- +-----------------------------------------------------+---------------+
+-- 13 rows in set (0.00 sec)
+
+SELECT title, released_year FROM books
+WHERE released_year NOT IN 
+(2000,2002,2004,2006,2008,2010,2012,2014,2016);
+-- mysql> source Logical Operators/Notes.sql;
+-- +-----------------------------------------------------+---------------+
+-- | title                                               | released_year |
+-- +-----------------------------------------------------+---------------+
+-- | The Namesake                                        |          2003 |
+-- | American Gods                                       |          2001 |
+-- | Interpreter of Maladies                             |          1996 |
+-- | The Circle                                          |          2013 |
+-- | A Heartbreaking Work of Staggering Genius           |          2001 |
+-- | Coraline                                            |          2003 |
+-- | What We Talk About When We Talk About Love: Stories |          1981 |
+-- | Where I'm Calling From: Selected Stories            |          1989 |
+-- | White Noise                                         |          1985 |
+-- | Cannery Row                                         |          1945 |
+-- | Consider the Lobster                                |          2005 |
+-- | fake_book                                           |          2001 |
+-- | Lincoln In The Bardo                                |          2017 |
+-- +-----------------------------------------------------+---------------+
+-- 13 rows in set (0.00 sec)
+
+SELECT title, released_year FROM books
+WHERE released_year >= 2000
+AND released_year NOT IN 
+(2000,2002,2004,2006,2008,2010,2012,2014,2016);
+-- mysql> source Logical Operators/Notes.sql;
+-- +-------------------------------------------+---------------+
+-- | title                                     | released_year |
+-- +-------------------------------------------+---------------+
+-- | The Namesake                              |          2003 |
+-- | American Gods                             |          2001 |
+-- | The Circle                                |          2013 |
+-- | A Heartbreaking Work of Staggering Genius |          2001 |
+-- | Coraline                                  |          2003 |
+-- | Consider the Lobster                      |          2005 |
+-- | fake_book                                 |          2001 |
+-- | Lincoln In The Bardo                      |          2017 |
+-- +-------------------------------------------+---------------+
+-- 8 rows in set (0.00 sec)
+
+SELECT title, released_year FROM books
+WHERE released_year >= 2000 AND
+released_year % 2 != 0 ORDER BY released_year;   --bisa di combine dengan operator mathematic logic nya
+-- # mysql> source Logical Operators/Notes.sql;
+-- # +-------------------------------------------+---------------+
+-- # | title                                     | released_year |
+-- # +-------------------------------------------+---------------+
+-- # | American Gods                             |          2001 |
+-- # | A Heartbreaking Work of Staggering Genius |          2001 |
+-- # | fake_book                                 |          2001 |
+-- # | The Namesake                              |          2003 |
+-- # | Coraline                                  |          2003 |
+-- # | Consider the Lobster                      |          2005 |
+-- # | The Circle                                |          2013 |
+-- # | Lincoln In The Bardo                      |          2017 |
+-- # +-------------------------------------------+---------------+
+-- # 8 rows in set (0.00 sec)
